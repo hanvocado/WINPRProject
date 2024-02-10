2da250d25fb1e0ef8d1db91d5fc0ee9264bca1c6
hoang_nguyen <nguyenhoang692004@gmail.com>
2024-02-10 19:32:29 +0700

    HomePage,TopicPage,StudentPage - Design UI

--- ThesisManagement/Views/Professor/UserControls/StudentUC.xaml.cs
+++ ThesisManagement/Views/Professor/UserControls/StudentUC.xaml.cs
@@ -10,15 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ThesisManagement.Views.Professor
+namespace ThesisManagement.Views.Professor.UserControls
 {
-    public partial class ProfessorMainView : Window
+    /// <summary>
+    /// Interaction logic for StudentUC.xaml
+    /// </summary>
+    public partial class StudentUC : UserControl
     {
-        public ProfessorMainView()
+        public StudentUC()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
     }
 }
--- ThesisManagement/Views/Professor/UserControls/TopicUC.xaml.cs
+++ ThesisManagement/Views/Professor/UserControls/TopicUC.xaml.cs
@@ -10,13 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ThesisManagement.Views.Professor
+namespace ThesisManagement.Views.Professor.UserControls
 {
-    public partial class ProfessorMainView : Window
+    /// <summary>
+    /// Interaction logic for TopicUC.xaml
+    /// </summary>
+    public partial class TopicUC : UserControl
     {
-        public ProfessorMainView()
+        public TopicUC()
         {
             InitializeComponent();
         }
