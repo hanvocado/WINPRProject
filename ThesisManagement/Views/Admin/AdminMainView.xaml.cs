b1650f21ab4de643e5ce36d2e57644b5cf3d9e89
ntnhtc <han.ntnhtc@gmail.com>
2024-02-14 23:39:05 +0700

    create window control panel - AdminMainView

--- ThesisManagement/Views/Admin/AdminMainView.xaml.cs
+++ ThesisManagement/Views/Admin/AdminMainView.xaml.cs
@@ -1,27 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace ThesisManagement.Views.Admin
 {
-    /// <summary>
-    /// Interaction logic for AdminMainView.xaml
-    /// </summary>
     public partial class AdminMainView : Window
     {
         public AdminMainView()
         {
             InitializeComponent();
+            // Add a handler for the MouseLeftButtonDown event with handledEventsToo set to true
+            this.AddHandler(UIElement.MouseLeftButtonDownEvent, new MouseButtonEventHandler(OnMouseLeftButtonDown), true);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        // Event handler for the MouseLeftButtonDown event Drag and Move application
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
