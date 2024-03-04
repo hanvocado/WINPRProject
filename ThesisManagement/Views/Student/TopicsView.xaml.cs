44a31e25ee030985315f653301d7857e9c8fcf12
ntnhtc <han.ntnhtc@gmail.com>
2024-03-04 23:01:01 +0700

    rename TopicListView to TopicsView

---
+++ ThesisManagement/Views/Student/TopicsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThesisManagement.Views.Student
+{
+    /// <summary>
+    /// Interaction logic for TopicListView.xaml
+    /// </summary>
+    public partial class TopicsView : UserControl
+    {
+        public TopicsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
