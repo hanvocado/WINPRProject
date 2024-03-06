2162c9eb0bd68bf791ca99592de84d1fbb6ebccc
ntnhtc <han.ntnhtc@gmail.com>
2024-03-06 11:46:58 +0700

    Created PopupControlBar

---
+++ ThesisManagement/CustomControls/PopupControlBar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace ThesisManagement.CustomControls
+{
+    public partial class PopupControlBar : UserControl
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        public PopupControlBar()
+        {
+            InitializeComponent();
+            this.AddHandler(UIElement.MouseLeftButtonDownEvent, new MouseButtonEventHandler(OnMouseLeftButtonDown), true);
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(Window.GetWindow(this));
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+    }
+}
