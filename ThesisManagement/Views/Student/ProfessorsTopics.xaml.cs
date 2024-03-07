0226ae56f5100cb4d8b06ef7aca4622f1d394737
Vo Van Nam <vovannam220304@gmail.com>
2024-03-07 21:15:41 +0700

    topics

--- ThesisManagement/Views/Student/ProfessorsTopics.xaml.cs
+++ ThesisManagement/Views/Student/ProfessorsTopics.xaml.cs
@@ -24,5 +24,24 @@ namespace ThesisManagement.Views.Student
         {
             InitializeComponent();
         }
+        private void ListViewItem_Click(object sender, RoutedEventArgs e)
+        {
+            //var listView = sender as ListView;
+            //var selectedItem = listView?.SelectedItem;
+            //if (selectedItem != null)
+            //{
+            //    Topic topic = selectedItem as Topic;
+            //    TopicView topicView = new();
+            //    topicView.DataContext = new TopicVM
+            //    {
+            //        Name = topic.Name,
+            //        Category = topic.Category,
+            //        Technology = topic.Technology,
+            //        Description = topic.Description
+            //    };
+            //    topicView.Owner = Application.Current.MainWindow;
+            //    topicView.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            //    topicView.Show();
+        }
     }
 }
