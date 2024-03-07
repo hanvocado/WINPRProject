5cea679092fae33b94a6948c5535bb30fcad7d5e
hoang_nguyen <nguyenhoang692004@gmail.com>
2024-03-08 01:21:01 +0700

    fix delete topic

--- ThesisManagement/Views/Professor/TopicsView.xaml.cs
+++ ThesisManagement/Views/Professor/TopicsView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using ThesisManagement.Models;
 using ThesisManagement.ViewModels;
 
@@ -53,6 +54,27 @@ namespace ThesisManagement.Views.Professor
         {
             var topic = (Topic)obj;
             return topic.Name.Contains(FilterTexbox.Text, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var listViewItem = sender as ListViewItem;
+            var topic = listViewItem?.DataContext as Topic;
+            if (topic != null)
+            {
+                TopicVM topicVM = new TopicVM
+                {
+                    SelectedTopic = new Topic
+                    {
+                        Id = topic.Id,
+                        Name = topic.Name,
+                        Category = topic.Category,
+                        Technology = topic.Technology,
+                        Description = topic.Description
+                    }
+                };
+               //MessageBox.Show($"{topicVM.SelectedTopic.Id},{topicVM.SelectedTopic.Name} ,  {topicVM.SelectedTopic.Category} ,  {topicVM.SelectedTopic.Technology} ,  {topicVM.SelectedTopic.Description}");
+            }
         }
     }
 }
