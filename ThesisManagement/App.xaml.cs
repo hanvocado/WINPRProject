dec0048d7e949e6a13e49ebb11c0ee73df912063
Vo Van Nam <vovannam220304@gmail.com>
2024-03-13 20:44:20 +0700

    UI

--- ThesisManagement/App.xaml.cs
+++ ThesisManagement/App.xaml.cs
@@ -13,7 +13,7 @@ namespace ThesisManagement
         {
             base.OnStartup(e);
 
-            ShowStudentWindow();
+            ShowProfessorWindow();
 
         }
 
--- ThesisManagement/Views/Professor/TopicsView.xaml.cs
+++ ThesisManagement/Views/Professor/TopicsView.xaml.cs
@@ -16,63 +16,6 @@ namespace ThesisManagement.Views.Professor
             InitializeComponent();
         }
 
-        private void ListViewItem_Click(object sender, RoutedEventArgs e)
-        {
-            var listView = sender as ListView;
-            var selectedItem = listView?.SelectedItem;
-
-            if (selectedItem != null)
-            {
-                Topic topic = selectedItem as Topic;
-                TopicView topicView = new TopicView();
-
-                TopicVM topicVM = new TopicVM
-                {
-                    SelectedTopic = new Topic
-                    {
-                        Name = topic.Name,
-                        Category = topic.Category,
-                        Technology = topic.Technology,
-                        Description = topic.Description
-                    }
-                };
-               // ShowMessage($"{topicVM.SelectedTopic.Name}, {topicVM.SelectedTopic.Category},{topicVM.SelectedTopic.Technology},{topicVM.SelectedTopic.Description}");
-
-                topicView.DataContext = topicVM;
-                topicView.Owner = Application.Current.MainWindow;
-                topicView.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-                topicView.Show();
-            }
-        }
-
-        private void FilterTexbox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TopicListView.Items.Filter = FilterMethod;
-        }
-
-        private bool FilterMethod(object obj)
-        {
-            var topic = (Topic)obj;
-            return topic.Name.Contains(FilterTexbox.Text, StringComparison.OrdinalIgnoreCase);
-        }
-
-        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
-        {
-            var listViewItem = sender as ListViewItem;
-            var topic = listViewItem?.DataContext as Topic;
-            TopicsViewModel dataContext = this.DataContext as TopicsViewModel ?? new TopicsViewModel();
-            if (topic != null)
-            {
-                dataContext.SelectedTopic = new Topic
-                {
-                    Id = topic.Id,
-                    Name = topic.Name,
-                    Category = topic.Category,
-                    Technology = topic.Technology,
-                    Description = topic.Description
-                };
-                //MessageBox.Show($"{topicVM.SelectedTopic.Id},{topicVM.SelectedTopic.Name} ,  {topicVM.SelectedTopic.Category} ,  {topicVM.SelectedTopic.Technology} ,  {topicVM.SelectedTopic.Description}");
-            }
-        }
+        
     }
 }
